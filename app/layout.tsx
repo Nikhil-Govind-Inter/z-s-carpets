9eae2fc150f2a74fff1108cd1e5beb5b053b5fcb
Nikhil-Govind-Inter <nikhil@intersmart.in>
2026-05-07 11:46:43 +0530

    added claudeflare configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "../components/header";
 import Footer from "@/components/footer";
 
+export const runtime = 'edge';
 
 const manrope = Manrope({
   subsets: ["latin"],
